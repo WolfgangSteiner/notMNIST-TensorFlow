feac7111784e0f767646dd8891714a97490b1fcf
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-09-05 17:40:25 +0200

    add scripts to download/prepare data

---
+++ download_data.sh
@@ -0,0 +1,2 @@
+wget http://commondatastorage.googleapis.com/books1000/notMNIST_large.tar.gz
+wget http://commondatastorage.googleapis.com/books1000/notMNIST_small.tar.gz
---
+++ prepare_data.py
@@ -0,0 +1,338 @@
+
+# coding: utf-8
+
+# Deep Learning
+# =============
+#
+# Assignment 1
+# ------------
+#
+# The objective of this assignment is to learn about simple data curation practices, and familiarize you with some of the data we'll be reusing later.
+#
+# This notebook uses the [notMNIST](http://yaroslavvb.blogspot.com/2011/09/notmnist-dataset.html) dataset to be used with python experiments. This dataset is designed to look like the classic [MNIST](http://yann.lecun.com/exdb/mnist/) dataset, while looking a little more like real data: it's a harder task, and the data is a lot less 'clean' than MNIST.
+
+# In[7]:
+
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
+def check_download(filename, expected_bytes, force=False):
+  statinfo = os.stat(filename)
+  if statinfo.st_size == expected_bytes:
+    print('Found and verified', filename)
+  else:
+    raise Exception(
+      'Failed to verify ' + filename + '. Can you get to it with a browser?')
+  return filename
+
+train_filename = check_download('notMNIST_large.tar.gz', 247336696)
+test_filename = check_download('notMNIST_small.tar.gz', 8458043)
+
+# Extract the dataset from the compressed .tar.gz file.
+# This should give you a set of directories, labelled A through J.
+
+# In[12]:
+
+num_classes = 10
+np.random.seed(133)
+
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  data_folders = [
+    os.path.join(root, d) for d in sorted(os.listdir(root))
+    if os.path.isdir(os.path.join(root, d))]
+  if len(data_folders) != num_classes:
+    raise Exception(
+      'Expected %d folders, one per class. Found %d instead.' % (
+        num_classes, len(data_folders)))
+  print(data_folders)
+  return data_folders
+
+train_folders = maybe_extract(train_filename)
+test_folders = maybe_extract(test_filename)
+
+
+# ---
+# Problem 1
+# ---------
+#
+# Let's take a peek at some of the data to make sure it looks sensible. Each exemplar should be an image of a character A through J rendered in a different font. Display a sample of the images that we just downloaded. Hint: you can use the package IPython.display.
+#
+
+# In[13]:
+
+display(Image("notMNIST_small/A/MDEtMDEtMDAudHRm.png"))
+
+
+# Now let's load the data in a more manageable format. Since, depending on your computer setup you might not be able to fit it all in memory, we'll load each class into a separate dataset, store them on disk and curate them independently. Later we'll merge them into a single dataset of manageable size.
+#
+# We'll convert the entire dataset into a 3D array (image index, x, y) of floating point values, normalized to have approximately zero mean and standard deviation ~0.5 to make training easier down the road.
+#
+# A few images might not be readable, we'll just skip them.
+
+# In[24]:
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+def load_letter(folder, min_num_images):
+  """Load the data for a single letter label."""
+  image_files = os.listdir(folder)
+  dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                         dtype=np.float32)
+  print(folder)
+  num_images = 0
+  for image in image_files:
+    image_file = os.path.join(folder, image)
+    try:
+      image_data = (ndimage.imread(image_file).astype(float) -
+                    pixel_depth / 2) / pixel_depth
+      if image_data.shape != (image_size, image_size):
+        raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+      dataset[num_images, :, :] = image_data
+      num_images = num_images + 1
+    except IOError as e:
+      print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+
+  dataset = dataset[0:num_images, :, :]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' %
+                    (num_images, min_num_images))
+
+  print('Full dataset tensor:', dataset.shape)
+  print('Mean:', np.mean(dataset))
+  print('Standard deviation:', np.std(dataset))
+  return dataset
+
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+  dataset_names = []
+  for folder in data_folders:
+    set_filename = folder + '.pickle'
+    dataset_names.append(set_filename)
+    if os.path.exists(set_filename) and not force:
+      # You may override by setting force=True.
+      print('%s already present - Skipping pickling.' % set_filename)
+    else:
+      print('Pickling %s.' % set_filename)
+      dataset = load_letter(folder, min_num_images_per_class)
+      try:
+        with open(set_filename, 'wb') as f:
+          pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+      except Exception as e:
+        print('Unable to save data to', set_filename, ':', e)
+
+  return dataset_names
+
+train_datasets = maybe_pickle(train_folders, 45000)
+test_datasets = maybe_pickle(test_folders, 1800)
+
+
+# ---
+# Problem 2
+# ---------
+#
+# Let's verify that the data still looks good. Displaying a sample of the labels and images from the ndarray. Hint: you can use matplotlib.pyplot.
+#
+# ---
+
+# In[35]:
+
+import random
+
+a = random.choice(pickle.load(open(random.choice(train_datasets), "rb" )))
+imgplot = plt.imshow(a)
+
+
+# ---
+# Problem 3
+# ---------
+# Another check: we expect the data to be balanced across classes. Verify that.
+#
+# ---
+
+# In[42]:
+
+for f in train_datasets:
+    print(f)
+    print(len(pickle.load(open(f, "rb"))))
+
+
+# Merge and prune the training data as needed. Depending on your computer setup, you might not be able to fit it all in memory, and you can tune `train_size` as needed. The labels will be stored into a separate array of integers 0 through 9.
+#
+# Also create a validation dataset for hyperparameter tuning.
+
+# In[43]:
+
+def make_arrays(nb_rows, img_size):
+  if nb_rows:
+    dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+    labels = np.ndarray(nb_rows, dtype=np.int32)
+  else:
+    dataset, labels = None, None
+  return dataset, labels
+
+def merge_datasets(pickle_files, train_size, valid_size=0):
+  num_classes = len(pickle_files)
+  valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+  train_dataset, train_labels = make_arrays(train_size, image_size)
+  vsize_per_class = valid_size // num_classes
+  tsize_per_class = train_size // num_classes
+
+  start_v, start_t = 0, 0
+  end_v, end_t = vsize_per_class, tsize_per_class
+  end_l = vsize_per_class+tsize_per_class
+  for label, pickle_file in enumerate(pickle_files):
+    try:
+      with open(pickle_file, 'rb') as f:
+        letter_set = pickle.load(f)
+        # let's shuffle the letters to have random validation and training set
+        np.random.shuffle(letter_set)
+        if valid_dataset is not None:
+          valid_letter = letter_set[:vsize_per_class, :, :]
+          valid_dataset[start_v:end_v, :, :] = valid_letter
+          valid_labels[start_v:end_v] = label
+          start_v += vsize_per_class
+          end_v += vsize_per_class
+
+        train_letter = letter_set[vsize_per_class:end_l, :, :]
+        train_dataset[start_t:end_t, :, :] = train_letter
+        train_labels[start_t:end_t] = label
+        start_t += tsize_per_class
+        end_t += tsize_per_class
+    except Exception as e:
+      print('Unable to process data from', pickle_file, ':', e)
+      raise
+
+  return valid_dataset, valid_labels, train_dataset, train_labels
+
+
+train_size = 200000
+valid_size = 10000
+test_size = 10000
+
+valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(
+  train_datasets, train_size, valid_size)
+_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+
+print('Training:', train_dataset.shape, train_labels.shape)
+print('Validation:', valid_dataset.shape, valid_labels.shape)
+print('Testing:', test_dataset.shape, test_labels.shape)
+
+
+# In[ ]:
+
+
+
+
+# Next, we'll randomize the data. It's important to have the labels well shuffled for the training and test distributions to match.
+
+# In[44]:
+
+def randomize(dataset, labels):
+  permutation = np.random.permutation(labels.shape[0])
+  shuffled_dataset = dataset[permutation,:,:]
+  shuffled_labels = labels[permutation]
+  return shuffled_dataset, shuffled_labels
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
+
+# In[ ]:
+
+
+
+
+# ---
+# Problem 4
+# ---------
+# Convince yourself that the data is still good after shuffling!
+#
+# ---
+
+# In[63]:
+
+index = random.randint(1,len(valid_dataset))
+print ("A B C D E F G H I J".split(' ')[valid_labels[index]])
+plt.imshow(valid_dataset[index])
+
+
+# Finally, let's save the data for later reuse:
+
+# In[64]:
+
+pickle_file = 'notMNIST.pickle'
+
+try:
+  f = open(pickle_file, 'wb')
+  save = {
+    'train_dataset': train_dataset,
+    'train_labels': train_labels,
+    'valid_dataset': valid_dataset,
+    'valid_labels': valid_labels,
+    'test_dataset': test_dataset,
+    'test_labels': test_labels,
+    }
+  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+  f.close()
+except Exception as e:
+  print('Unable to save data to', pickle_file, ':', e)
+  raise
+
+
+# In[66]:
+
+statinfo = os.stat(pickle_file)
+print('Compressed pickle size:', statinfo.st_size / 1024 / 1024, " mb")
+
+
+# ---
+# Problem 5
+# ---------
+#
+# By construction, this dataset might contain a lot of overlapping samples, including training data that's also contained in the validation and test set! Overlap between training and test can skew the results if you expect to use your model in an environment where there is never an overlap, but are actually ok if you expect to see training samples recur when you use it.
+# Measure how much overlap there is between training, validation and test samples.
+#
+# Optional questions:
+# - What about near duplicates between datasets? (images that are almost identical)
+# - Create a sanitized validation and test set, and compare your accuracy on those in subsequent assignments.
+# ---
+
+# In[73]:
+
+dict = {}
+for x in train_dataset:
+    dict[x.tostring()] = True
+
+valid_count = 0
+for x_v in valid_dataset:
+    if dict.has_key(x_v.tostring()):
+        valid_count+=1
+
+test_count = 0
+for x_t in test_dataset:
+    if dict.has_key(x_t.tostring()):
+        test_count+=1
+
+print("Overlap for validation set: ", float(valid_count)/len(valid_dataset))
+print("Overlap for test set: ", float(test_count)/len(test_dataset))
